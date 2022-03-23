eb0fbe23144ac96b029c6a6860f7d91d4cf71ab5
shravani <shravani.mula@thundersoft.com>
2022-03-23 17:48:43 +0530

    bubble sort

---
+++ bubblesort.c
@@ -0,0 +1,37 @@
+#include<stdio.h>    
+void display(int a[], int n) 
+{  
+	int i;  
+	for(i=0;i<n;i++)    
+	{    
+		printf("%d ",a[i]);    
+	}        
+}  
+void bubble(int a[], int n)   
+{  
+	int i, j, temp;  
+	for(i=0;i<n;i++)    
+	{    
+		for(j=i+1;j<n;j++) 
+		{
+			if(a[j]<a[i])    
+			{    
+				temp=a[i];    
+				a[i]=a[j];    
+				a[j]=temp;     
+			}     
+		}     
+	}     
+}  
+int main ()    
+{    
+	int i, j,temp;     
+	int a[5]={ 10, 35, 32, 13, 26};     
+	int n=sizeof(a)/sizeof(a[0]);   
+	printf("Before sorting array elements are:\n");  
+	display(a, n);  
+	bubble(a, n);  
+	printf("\nAfter sorting array elements are:\n");    
+	display(a, n);  
+}    
+
